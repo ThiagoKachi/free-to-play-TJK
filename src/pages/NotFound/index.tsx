6885f8448efddf09324541d4061c651a50951d94
Thiago Kachinsky <tjk091@gmail.com>
2021-09-28 10:54:05 -0300

    Footer and create page not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.scss';
+
+import errorImg from '../../assets/errorImg.png';
 
 export function NotFound() {
-  return <p>Error 404</p>;
+  return (
+    <div className="page-not-found">
+      <img src={errorImg} alt="Page not found" />
+      <Link to="/">Voltar a Home</Link>
+    </div>
+  );
 }
